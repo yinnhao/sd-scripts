20e14b951eb4327c561ccf5845f2e7bd4d6c51bc
zhuyinghao <zhuyinghao@baidu.com>
2025-01-08 20:07:33 +0800

    add ddpm

--- diffusers_quicktour/models_and_schedulers.py
+++ diffusers_quicktour/models_and_schedulers.py
@@ -1,6 +1,20 @@
 from diffusers import UNet2DModel
 from diffusers import DDPMScheduler
 import torch
+import tqdm
+import PIL.Image
+import numpy as np
+
+
+def display_sample(sample, i):
+    image_processed = sample.cpu().permute(0, 2, 3, 1)
+    image_processed = (image_processed + 1.0) * 127.5
+    image_processed = image_processed.numpy().astype(np.uint8)
+
+    image_pil = PIL.Image.fromarray(image_processed[0])
+    # display(f"Image at step {i}")
+    # display(image_pil)
+
 torch.manual_seed(0)
 
 repo_id = "google/ddpm-cat-256"
@@ -16,5 +30,26 @@ with torch.no_grad():
 # print(noisy_residual.shape)
 print(type(noisy_residual))
 
-# scheduler = DDPMScheduler.from_pretrained(repo_id)
-# print(scheduler)
+# 定义DDPMScheduler
+scheduler = DDPMScheduler.from_pretrained(repo_id)
+print(scheduler)
+
+# 执行一步去噪
+less_noisy_sample = scheduler.step(model_output=noisy_residual, timestep=2, sample=noisy_sample).prev_sample
+
+# 将输入噪声和模型移动到现存上
+model.to("cuda")
+noisy_sample = noisy_sample.to("cuda")
+
+sample = noisy_sample
+for i, t in enumerate(tqdm.tqdm(scheduler.timesteps)):
+    # 1. predict noise residual
+    with torch.no_grad():
+        residual = model(sample, t).sample
+
+    # 2. compute less noisy image and set x_t -> x_t-1
+    sample = scheduler.step(residual, t, sample).prev_sample
+
+    # 3. optionally look at image
+    if (i + 1) % 50 == 0:
+        display_sample(sample, i + 1)
